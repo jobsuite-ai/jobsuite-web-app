46ad0920b3e6ac87e0acbd7b4280195311309d9a
jonaspeekpainting <jonas@rlpeekpainting.com>
2025-04-01 16:10:22 -0700

    Log when status gets updated

--- components/Global/updateJobStatus.ts
+++ components/Global/updateJobStatus.ts
@@ -1,10 +1,14 @@
 'use client';
 
+import { useUser } from '@auth0/nextjs-auth0/client';
+
 import { JobStatus } from './model';
 
 import { UpdateJobContent } from '@/app/api/jobs/jobTypes';
+import { logToCloudWatch } from '@/public/logger';
 
 export default async function updateJobStatus(status: JobStatus, jobID: string) {
+    const { user } = useUser();
     const content: UpdateJobContent = {
         job_status: status,
     };
@@ -21,5 +25,7 @@ export default async function updateJobStatus(status: JobStatus, jobID: string) 
     );
 
     const { Attributes } = await response.json();
+
+    logToCloudWatch(`Job status for jog with id: ${jobID} updated to: ${status} by: ${user?.email}`);
     return Attributes;
 }
