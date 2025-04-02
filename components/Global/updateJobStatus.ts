89b760536a457e2019b89437d7e45a57e80d4ec7
jonaspeekpainting <jonas@rlpeekpainting.com>
2025-04-02 09:34:48 -0700

    Change how we are logging when updating status

--- components/Global/updateJobStatus.ts
+++ components/Global/updateJobStatus.ts
@@ -1,14 +1,11 @@
 'use client';
 
-import { useUser } from '@auth0/nextjs-auth0/client';
-
 import { JobStatus } from './model';
 
 import { UpdateJobContent } from '@/app/api/jobs/jobTypes';
 import { logToCloudWatch } from '@/public/logger';
 
 export default async function updateJobStatus(status: JobStatus, jobID: string) {
-    const { user } = useUser();
     const content: UpdateJobContent = {
         job_status: status,
     };
@@ -24,8 +21,7 @@ export default async function updateJobStatus(status: JobStatus, jobID: string) 
         }
     );
 
+    await logToCloudWatch(`Job status for jog with id: ${jobID} updated to: ${status}`);
     const { Attributes } = await response.json();
-
-    logToCloudWatch(`Job status for jog with id: ${jobID} updated to: ${status} by: ${user?.email}`);
     return Attributes;
 }
