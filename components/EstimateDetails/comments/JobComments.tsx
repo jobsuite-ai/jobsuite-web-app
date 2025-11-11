1e2f3a2238d987b0e3965992fc0b7bac695315bd
jonaspeekpainting <jonas@rlpeekpainting.com>
2025-11-10 19:56:57 -0800

    Fix linting 2

--- components/EstimateDetails/comments/JobComments.tsx
+++ components/EstimateDetails/comments/JobComments.tsx
@@ -38,7 +38,6 @@ export default function JobComments({ estimateID }: { estimateID: string }) {
     useEffect(() => {
         setLoading(true);
         getJobComments().finally(() => setLoading(false));
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [estimateID]);
 
     async function postJobComment() {
