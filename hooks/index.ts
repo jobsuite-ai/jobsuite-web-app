67a20bf68119214bd122b6e4868dc54fc3c30db2
jonaspeekpainting <jonas@rlpeekpainting.com>
2025-12-13 08:46:37 -0800

    Use logo for header

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,2 +1,3 @@
 export { useAuth, type User, type UseAuthOptions, type UseAuthReturn } from './useAuth';
 export { useUsers, invalidateUsersCache } from './useUsers';
+export { useContractorLogo } from './useContractorLogo';
