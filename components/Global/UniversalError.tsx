f88efae5b5c35ef07d19900d3b59d589798e1a83
jonaspeekpainting <jonas@rlpeekpainting.com>
2026-02-06 16:09:32 -0800

    Fix notification displayes

--- components/Global/UniversalError.tsx
+++ components/Global/UniversalError.tsx
@@ -1,11 +1,16 @@
-import { Flex } from '@mantine/core';
+import { Flex, Text } from '@mantine/core';
 import { IconExclamationCircle } from '@tabler/icons-react';
 
 export default function UniversalError({ message = 'Unexpected Error' }: { message: string }) {
     return (
-        <Flex direction="column" align="center">
-            <IconExclamationCircle color="#FF7F7F" size={180} />
-            <h2>{message}</h2>
+        <Flex
+          direction="column"
+          align="center"
+          justify="center"
+          style={{ minHeight: '70vh', width: '100%' }}
+        >
+            <IconExclamationCircle color="#FF7F7F" size={140} />
+            <Text c="gray.0" size="xl" fw={700}>{message}</Text>
         </Flex>
     );
 }
