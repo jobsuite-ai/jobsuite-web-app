a121a5c8eedff969b143afa2502e3136921ee863
jonaspeekpainting <jonas@rlpeekpainting.com>
2026-03-31 16:09:20 -0700

    Fix failing tests

--- app/api/schedule/[schedule_id]/route.ts
+++ app/api/schedule/[schedule_id]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-import { getContractorId } from '../utils/getContractorId';
+import { getContractorId } from '../../utils/getContractorId';
 
 import { getApiBaseUrl } from '@/app/api/utils/serviceAuth';
 
